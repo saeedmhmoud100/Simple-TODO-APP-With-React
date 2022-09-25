bca239becc53b46c667cab6e698d6877a892dcce
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-09-25 23:43:10 +0200

    get users with react axios

--- src/App.js
+++ src/App.js
@@ -5,38 +5,75 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import axios from "axios";
+// class App extends Component {
+//   state = {
+//     Data: [
+//       { name: "Ahmed", age: 28, job: "work" },
+//       { name: "Ali", age: 30, job: "Developer" },
+//       { name: "Mohamed", age: 20, job: "Designer" },
+//       { name: "Osama", age: 40, job: "Freelancer" }
+//     ]
+//   };
+
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <div>
+//           <Nav />
+//           <div>
+//             {/* <Route path="/Component/About" component={About} />
+//           <Route path="/Component/Home" component={Home} /> */}
+//             {/* <Link to={"./Home"}>
+//             <Home />
+//           </Link>
+//           <Link to={"./About"}>
+//             <About />
+//           </Link> */}
+//           </div>
+//           <Routes>
+//             <Route path="/Component/Home" element={<Home />} />
+//             <Route path="/Component/About/" element={<About />} />
+//             <Route
+//               path="/"
+//               element={
+//                 <div className={"container"}>
+//                   {this.state.Data.map((n, i) =>
+//                     <Item key={i} name={n.name} age={n.age} job={n.job} />
+//                   )}
+//                 </div>
+//               }
+//             />
+//           </Routes>
+//         </div>
+//       </BrowserRouter>
+//     );
+//   }
+// }
+
 class App extends Component {
   state = {
-    Data: [
-      { name: "Ahmed", age: 28, job: "work" },
-      { name: "Ali", age: 30, job: "Developer" },
-      { name: "Mohamed", age: 20, job: "Designer" },
-      { name: "Osama", age: 40, job: "Freelancer" }
-    ]
+    users: []
   };
 
+  componentDidMount = _ => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(n => {
+      this.setState({ users: Array.from(n.data) });
+      console.log(this.state.users);
+    });
+  };
   render() {
     return (
       <BrowserRouter>
         <div>
           <Nav />
-          <div>
-            {/* <Route path="/Component/About" component={About} />
-          <Route path="/Component/Home" component={Home} /> */}
-            {/* <Link to={"./Home"}>
-            <Home />
-          </Link>
-          <Link to={"./About"}>
-            <About />
-          </Link> */}
-          </div>
           <Routes>
             <Route path="/Component/Home" element={<Home />} />
             <Route path="/Component/About/" element={<About />} />
           </Routes>
           <div className={"container"}>
-            {this.state.Data.map((n, i) =>
-              <Item key={i} name={n.name} age={n.age} job={n.job} />
+            {this.state.users.map((n, i) =>
+              <Item key={i} name={n.name} age={n.username} job={n.email} />
             )}
           </div>
         </div>
@@ -44,21 +81,5 @@ class App extends Component {
     );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <BrowserRouter>
-//         <div>
-//           <Nav />
-//           <Routes>
-//             <Route path="/Component/Home" element={<Home />} />
-//             <Route path="/Component/About/" element={<About />} />
-//           </Routes>
-//         </div>
-//       </BrowserRouter>
-//     );
-//   }
-// }
 
 export default App;
